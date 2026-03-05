5d513dcdfa392d4ddbfff6642140f359333f6961
cooksoundscapes <paulobmedina@gmail.com>
2026-03-05 19:08:23 -0300

    namespace and gitignore

--- include/core/utils.hpp
+++ include/core/utils.hpp
@@ -1,14 +1,15 @@
 #pragma once
 #include <iostream>
 
-template <typename... Args>
-void print(Args... args) {
-    auto separator = "";
-    ((std::cout << separator << args, separator = " "), ...);
-    std::cout << std::endl;
+namespace core_utils
+{
+
+    template <typename... Args>
+    void print(Args... args) {
+        auto separator = "";
+        ((std::cout << separator << args, separator = " "), ...);
+        std::cout << std::endl;
+    }
+
 }
 
-int main() {
-    print("Olá", 2026, 3.14, "C++ é vida");
-    return 0;
-}
